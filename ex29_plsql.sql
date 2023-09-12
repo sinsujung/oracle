f10f229b784430d360710e51a8a80793f7b1f12d
suding <jhw02045@naver.com>
2023-09-12 17:55:18 +0900

    0912

--- ex29_plsql.sql
+++ ex29_plsql.sql
@@ -1711,3 +1711,359 @@ insert into tblBoard values (seqBoard.nextVal, '또 다시 글을 씁니다.', '
 delete from tblBoard where seq = 5;
 
 
+/*
+
+    g함수 return
+    
+    1. 단일값 O
+    2. 다중값 > cursor
+    프로시저 out parameter
+    
+    1. 단일값(단일 레코드)
+        a. number
+        b. varchar2
+        c. date
+        
+    2. 다중값(다중 레코드)\
+        a. cursor
+
+*/
+set serveroutput on;
+create or replace procedure procBuseo(
+    pbuseo varchar2
+)
+is
+    cursor vcursor
+    is
+    select * from tblinsa where buseo = pbuseo;
+    
+    vrow tblinsa%rowtype;
+begin
+
+    open vcursor;
+    loop
+        fetch vcursor into vrow; --select into
+        exit when vcursor%notfound;
+        
+        --업무
+        dbms_output.put_line(vrow.name || ',' || vrow.buseo);
+        
+    end loop;
+    close vcursor;
+end procBuseo;
+
+begin
+    procbuseo('영업부');
+end;
+
+---------------
+create or replace procedure procbuseo(
+    pbuseo in varchar2,
+    pcursor out sys_refcursor -- 커서의 자료형
+)
+is
+begin
+
+    open pcursor
+    for
+    select * from tblinsa where buseo = pbuseo;
+    
+end procbuseo;
+
+declare
+    vcursor sys_refcursor;--커서 참조 변수 
+    vrow tblinsa%rowtype;
+begin
+
+    procbuseo('영업부', vcursor);
+    
+    loop
+        fetch vcursor into vrow;
+        exit when vcursor%notfound;
+        
+        --업무
+        dbms_output.put_line(vrow.name);
+        
+    end loop;
+end;
+
+
+-- 프로시저 총 정리 > CRUD
+
+--1. 추가 작업(C)
+create or replace procedure 추가작업(
+    추가할 데이터 -> in 매개변수,
+    추가할 데이터 -> in 매개변수,
+    추가할 데이터 -> in 매개변수, --원하는 만큼
+    성공 유무 반환 -> out 매개변수 -- 피트백(1,0)
+)
+is
+    내부 변수 선언
+begin
+    작업(insert + (select, update, delete))
+exception
+    when others then
+        예외처리
+end;
+
+-- 할일 추가하기(C)
+select * from tbltodo; --23
+create or replace procedure procAddtodo(
+    ptitle varchar2,
+    presult out number --1 or 0
+    
+)
+is
+begin
+
+    insert into tbltodo(seq, title, adddate, completedate)
+        values(seqtodo.nextVal, ptitle, sysdate, null);
+        
+    presult := 1;
+exception
+    when others then
+        presult := 0; --실패
+end procAddtodo;
+
+create sequence seqtodo start with 24;
+
+
+declare
+    vresult number;
+begin
+    procAddtodo('새로운 할일입니다.', vresult);
+    dbms_output.put_line(vresult);
+end;
+
+select * from tbltodo;
+
+-- 2. 수정 작업(U)
+create or replace procedure 수정작업(
+    수정할 데이터 -> in 매개변수,
+    수정할 데이터 -> in 매개변수,
+    수정할 데이터 -> in 매개변수, --원하는 개수
+    식별자       -> in 매개변수 --where절에 사용할 PK or 데이터
+    성공 유무 반환 -> out 매개변수 -- 피트백(1,0)
+)
+is
+    내부 변수 선언
+begin
+    작업(update + (insert, update, delete, select..))
+exception
+    when others then   
+        예외처리
+end;
+
+-- 할 일 수정하기(U) > completedate > 채우기 > 할일 완료 처리하기
+create or replace procedure procCompletetodo(
+--    pcompletedate date > 수정할 날짜 > 지금 > sysdate 처리
+    pseq in number, --수정할 할일 번호
+    presult out number
+)
+is
+begin
+    update tbltodo set
+        completedate = sysdate
+            where seq = pseq;
+    presult := 1;
+exception
+    when others then
+        presult := 0;
+end procCompletetodo;
+
+
+declare
+    vresult number;
+begin
+    procCompletetodo(24, vresult);
+    dbms_output.put_line(vresult);
+end;
+
+
+select * from tbltodo;
+
+
+-- 3. 삭제 작업(D)
+create or replace procedure 삭제작업(
+    식별자 -> in 매개변수,
+    성공 유무 반환 -> out 매개변수
+)
+is
+     내부 변수 선언
+begin
+    작업(delete + (insert, update, delete, select))
+exception
+    when others then
+        예외처리
+end;
+
+
+-- 할 일 삭제하기(D)
+create or replace procedure procDeletetodo(
+    pseq in number,
+    presult out number
+)
+is
+begin
+    delete from tbltodo where seq = pseq;
+    presult := 1;
+exception
+    when others then
+        presult := 0;
+end procDeletetodo;
+
+declare
+    vresult number;
+begin
+    procDeletetodo(24, vresult);
+    dbms_output.put_line(vresult);
+end;
+
+select * from tbltodo;
+
+
+-- 4. 읽기 작업(R)
+-- : 조건 유무/
+-- : 반환 단일행/다중행, 단일컬럼/다중컬럼
+
+-- 한일 몇개? 안한일 몇개? 총 몇개?
+create or replace procedure 읽기작업(
+    조건 데이터 -> int 매개변수,
+    단일 반환값 -> out 매개변수,
+    다중 반환값 -> out 매개변수(커서)
+)
+is
+    내부 변수 선언
+begin
+    작업(select + (insert, update, delete, select))
+exception
+    when others then
+        예외처리
+end;
+
+-- 한일 몇개? 안한일 몇개? 총 몇개?
+create or replace procedure procCounttodo(
+    pcount1 out number, --한일
+    pcount2 out number, --안한일
+    pcount3 out number --모든일
+)
+is
+begin
+    select count(*) into pcount1 from tbltodo where completedate is not null;
+    select count(*) into pcount2 from tbltodo where completedate is null;
+    select count(*) into pcount3 from tbltodo;
+exception
+    when others then
+        dbms_output.put_line('예외 처리'); --실제로는 log를 남김
+end;
+
+declare
+    vcount1 number;
+    vcount2 number;
+    vcount3 number;
+begin
+    procCounttodo(vcount1, vcount2, vcount3);
+    dbms_output.put_line(vcount1);
+    dbms_output.put_line(vcount2);
+    dbms_output.put_line(vcount3);
+end;
+
+-----
+
+create or replace procedure procCounttodo(
+    psel in number,     --선택(1(한일), 2(안한일), 3(모든일))
+    pcount out number   -- 
+)
+is
+begin
+    if psel = 1 then
+        select count(*) into pcount from tbltodo where completedate is not null;
+    elsif psel = 2 then
+        select count(*) into pcount from tbltodo where completedate is null;
+    elsif psel = 3 then
+        select count(*) into pcount from tbltodo;
+    end if;
+exception
+    when others then
+        dbms_output.put_line('예외 처리'); --실제로는 log를 남김
+end;
+
+declare
+    vcount number;
+begin
+    procCounttodo(3, vcount);
+    dbms_output.put_line(vcount);
+end;
+
+-- 번호 > 할일 1개 반환
+create or replace procedure procGettodo(
+    pseq in number,
+    prow out tbltodo%rowtype
+)
+is
+begin
+
+    select * into prow from tbltodo where seq = pseq;
+exception
+    when others then
+        dbms_output.put_line('예외 처리');
+end;
+
+declare
+    vrow tbltodo%rowtype;
+begin
+    procGettodo(1, vrow);
+    dbms_output.put_line(vrow.title);
+end;
+
+-- 다중 레코드 반환
+-- 1. 한일 목록 반환
+-- 2. 안한일 목록 반환
+-- 3. 모든일 목록 반환
+create or replace procedure procListtodo(
+    psel in number,
+    pcursor out sys_refcursor
+)
+is
+begin
+
+    if psel = 1 then
+        open pcursor
+        for
+        select * from tbltodo where completedate is not null;
+    elsif psel = 2 then
+        open pcursor
+        for
+        select * from tbltodo where completedate is null;
+    elsif psel = 3 then
+        open pcursor
+        for
+        select * from tbltodo;
+    end if;
+exception
+    when others then
+        dbms_output.put_line('예외 처리');
+end procListtodo;
+
+declare
+    vcursor sys_refcursor;
+    vrow tbltodo%rowtype;
+begin
+    procListtodo(1, vcursor);
+    
+    loop
+        fetch vcursor into vrow;
+        exit when vcursor%notfound;
+        
+        dbms_output.put_line(vrow.title || ',' || vrow.completedate);
+        
+    end loop;
+    
+end;
+
+
+
+
+
+
+
--- localhost.hong.sql
+++ localhost.hong.sql
@@ -10,11 +10,49 @@ create table tbldata(
 );
 
 
+CREATE TABLE tblCourseReg (
+	courseReg_seq     NUMBER  primary key,
+	course_seq        NUMBER NOT NULL, 
+	student_seq       NUMBER NOT NULL, 
+	completeState_seq NUMBER NOT NULL,
+	complete           DATE   NULL ,
+    
+    constraint course_seq_fk foreign key (course_seq) references tblCourse(course_seq),
+    constraint student_seq_fk foreign key (student_seq) references tblStudent(student_seq),
+    constraint completeState_seq_fk foreign key (completeState_seq) references tblCompleteState(completeState_seq)
+
+); 
+
+create table tblEmployment (    
+employment Number Primary Key,
+companyName Varchar(50),
+insurance Varchar(50),
+hireDate Date,
+maintenance_seq Number Not Null
+); 
 
 
 
+CREATE TABLE tblHopeDuty (
+	hopeDuty_seq    NUMBER   PRIMARY KEY, 
+	hopeDutyfield   VARCHAR(255) NULL,    
+	hopeSalary      NUMBER       NULL,    
+	maintenance_seq NUMBER       NOT NULL,
+    
+    constraint hd_maintenance_seq_fk foreign key (maintenance_seq) references tblEmployment(employment)
+
+);
 
 
+drop sequence seqmaintenance;
+create SEQUENCE seqmaintenance;
+
+create table tblMaintenance(    
+maintenance_seq Number Primary Key
+);
+
+drop table tblAdmin;
+select * from tblmaintenance;
 
 
 
